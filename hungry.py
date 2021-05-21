d4d2f5380abea5950dc10d48c9ca2f555381cee7
agarlap <agarlap@g.clemson.edu>
2021-05-21 03:12:27 -0400

    MUFFIN TIME!

--- hungry.py
+++ hungry.py
@@ -6,5 +6,6 @@ if hungry == "yes":
     print("Here, eat this samosa!")
     print("And... Eat this pizza too!")
     print("Hold up! Eat this doughnut as well!")
+    print("And eat this pumpkin spice muffin too!")
 else:
     print("WHAT? DO YOUR HOMEWORK!")
